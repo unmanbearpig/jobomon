1194d69cff916f919e688c8efd6b1afd7e01e5a0
unmanbearpig <unmanbearpig@gmail.com>
2017-02-23 19:15:30 +0300

    Split feeds#create spec into two

--- spec/requests/feeds_spec.rb
+++ spec/requests/feeds_spec.rb
@@ -16,12 +16,14 @@ RSpec.describe "Feeds", type: :request do
   end
 
   describe "POST /feeds" do
-    it "creates a new feed and returns it" do
+    let(:feed_data) do
       feed_data = {
         title: 'hello',
         url: 'https://example.com/test'
       }
+    end
 
+    it "creates a new feed" do
       expect do
         post feeds_path('json'), feed: feed_data
       end.to change { Feed.count }.from(0).to(1)
@@ -30,11 +32,17 @@ RSpec.describe "Feeds", type: :request do
       expect(feed.title).to eq('hello')
       expect(feed.url).to eq('https://example.com/test')
 
+    end
+
+    it "returns the created feed" do
+      post feeds_path('json'), feed: feed_data
+
       expect(response).to have_http_status(200)
       expect(JSON.parse(response.body))
-        .to eq('feed' => {'id' => feed.id,
+        .to eq('feed' => {'id' => Feed.first.id,
                           'title' => 'hello',
                           'url' => 'https://example.com/test'})
+
     end
   end
 end
