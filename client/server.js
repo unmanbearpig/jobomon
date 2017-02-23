3d56d0206c7b9aeab3c0403661f9f4084a57c026
unmanbearpig <unmanbearpig@gmail.com>
2017-02-24 02:15:16 +0300

    Proxy API requests to Rails

--- client/server.js
+++ client/server.js
@@ -3,40 +3,42 @@ var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 
 new WebpackDevServer(webpack(config), {
-    publicPath: config.output.publicPath,
-    hot: true,
-    historyApiFallback: true,
-    // It suppress error shown in console, so it has to be set to false.
-    quiet: false,
-    // It suppress everything except error, so it has to be set to false as well
-    // to see success build.
-    noInfo: false,
-    stats: {
-      // Config for minimal console.log mess.
-      assets: false,
-      colors: true,
-      version: false,
-      hash: false,
-      timings: false,
-      chunks: false,
-      chunkModules: false
-    },
-    proxy: {
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true,
+  // It suppress error shown in console, so it has to be set to false.
+  quiet: false,
+  // It suppress everything except error, so it has to be set to false as well
+  // to see success build.
+  noInfo: false,
+  stats: {
+    // Config for minimal console.log mess.
+    assets: false,
+    colors: true,
+    version: false,
+    hash: false,
+    timings: false,
+    chunks: false,
+    chunkModules: false
+  },
+  proxy: {
     "/api": {
       "target": {
         "host": "localhost",
         "protocol": 'http:',
         "port": 3000
       },
-      ignorePath: true,
+      pathRewrite: {
+        '^/api':''
+      },
       changeOrigin: true,
       secure: false
     }
   }
 }).listen(9000, 'localhost', function (err) {
-    if (err) {
-        console.log(err);
-    }
+  if (err) {
+    console.log(err);
+  }
 
   console.log('Listening at localhost:9000');
 });
