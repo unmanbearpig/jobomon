f9feaeeb06ab19d7f499cce0718368bcb6b219f1
unmanbearpig <unmanbearpig@gmail.com>
2017-02-23 20:07:59 +0300

    Inline unnecessarily extracted method

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,6 +1,6 @@
 class FeedsController < ApiController
   def index
-    render json: feeds
+    render json: Feed.all
   end
 
   def create
@@ -12,10 +12,6 @@ class FeedsController < ApiController
   end
 
   private
-
-  def feeds
-    @feeds ||= Feed.all
-  end
 
   def feed_params
     params.require(:feed).permit(:title, :url)
