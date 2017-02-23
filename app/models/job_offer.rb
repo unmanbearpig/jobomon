3de47c3e7110f804ffea3dc2ff3343553bf3eece
unmanbearpig <unmanbearpig@gmail.com>
2017-02-23 23:32:42 +0300

    Validate uniqueness of job offers across sources

--- app/models/job_offer.rb
+++ app/models/job_offer.rb
@@ -1,6 +1,7 @@
 class JobOffer < ApplicationRecord
   belongs_to :source, touch: true
   validates :url, :source, presence: true, allow_blank: false
+  validates :url, uniqueness: { scope: :source_id }
 
   def self.from_entry(source_entry)
     new(source_entry.to_h)
--- spec/models/job_offer_spec.rb
+++ spec/models/job_offer_spec.rb
@@ -2,8 +2,22 @@ require 'rails_helper'
 require 'sources/entry'
 
 RSpec.describe JobOffer, type: :model do
-  it { should validate_presence_of :url }
-  it { should validate_presence_of :source }
+  describe "validations" do
+    subject { FactoryGirl.build(:job_offer) }
+
+    it { should validate_presence_of :url }
+    it { should validate_presence_of :source }
+    it { should validate_uniqueness_of(:url).scoped_to(:source_id) }
+
+    it "allows same urls on multiple sources" do
+      s1, s2 = FactoryGirl.create(:source), FactoryGirl.create(:source)
+
+      url = 'https://example.com'
+
+      FactoryGirl.create(:job_offer, source: s1, url: url)
+      expect(FactoryGirl.create(:job_offer, source: s2, url: url)).to be_valid
+    end
+  end
 
   describe ".from_entry" do
     it "initializes new record from attributes of the provided object" do
