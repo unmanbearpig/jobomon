0290347013dc202822b818855bee32484a2ec1ad
unmanbearpig <unmanbearpig@gmail.com>
2017-02-24 16:58:08 +0300

    Render job offer HTML

--- client/app/components/JobOffer.js
+++ client/app/components/JobOffer.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
+const createMarkup = (htmlContent) => {
+  return { __html: htmlContent };
+};
+
 const JobOffer = (jobOffer) => {
   return (
     <div>
       <h2>
         <a href={jobOffer.url}>{ jobOffer.title }</a>
       </h2>
-      <p>{ jobOffer.content }</p>
+      <p dangerouslySetInnerHTML={createMarkup(jobOffer.content)}></p>
     </div>
   );
 };
