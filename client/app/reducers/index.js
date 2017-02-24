3201b6f7fd5c87e906f8b93b4ead3fc0df88217d
unmanbearpig <unmanbearpig@gmail.com>
2017-02-24 12:31:40 +0300

    Delete unused reducer, 2 spaces indent

--- client/app/reducers/index.js
+++ client/app/reducers/index.js
@@ -2,28 +2,18 @@ import { routerReducer as routing } from 'react-router-redux';
 import { combineReducers } from 'redux';
 import * as types from '../actions/types';
 
-const filter = (state = '', action) => {
-    switch (action.type) {
-    case types.FILTER:
-        return action.filter;
-    default:
-        return state;
-    }
-};
-
 const feeds = (state = [], action) => {
-    switch (action.type) {
-    case types.RECEIVED_FEEDS:
-        return action.feeds;
-    default:
-        return state;
-    }
+  switch (action.type) {
+  case types.RECEIVED_FEEDS:
+    return action.feeds;
+  default:
+    return state;
+  }
 };
 
 const rootReducer = combineReducers({
-    filter,
-    feeds,
-    routing
+  feeds,
+  routing
 });
 
 export default rootReducer;
