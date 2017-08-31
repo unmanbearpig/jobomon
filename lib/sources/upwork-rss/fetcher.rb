3918e2a1c48c932240f59b75571a986fb0253ef6
unmanbearpig <unmanbearpig@gmail.com>
2017-08-31 23:14:36 +0300

    Add proxy support

--- lib/sources/upwork-rss/fetcher.rb
+++ lib/sources/upwork-rss/fetcher.rb
@@ -1,3 +1,5 @@
+require 'faraday_adapter_socks'
+
 module Sources
   class UpworkRSS
     class Fetcher
@@ -5,10 +7,18 @@ module Sources
         connection(url).get
       end
 
+      def self.proxy_settings
+        url = ENV['PROXY_URL']
+        return nil unless url
+
+        { uri: URI.parse(url) }
+      end
+
       def self.connection(url)
-        Faraday.new(url: url) do |conn|
+        Faraday.new(url: url, proxy: proxy_settings) do |conn|
+          conn.request :url_encoded
           conn.use FaradayMiddleware::FollowRedirects, limit: 3
-          conn.adapter :net_http
+          conn.adapter :net_http_socks
         end
       end
     end
