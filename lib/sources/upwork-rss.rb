16a6a11e7ec561f7ea0a42a76f15a5d3277abdc2
unmanbearpig <unmanbearpig@gmail.com>
2017-09-11 16:38:05 +0300

    Check for captcha in Upwork fetcher

--- lib/sources/upwork-rss.rb
+++ lib/sources/upwork-rss.rb
@@ -4,7 +4,28 @@ require_relative 'upwork-rss/parser'
 module Sources
   class UpworkRSS
     def self.fetch(url)
-      parser.parse_feed fetcher.fetch(url).body
+      response = fetcher.fetch(url)
+
+      unless response.success?
+        return raise_response_error(response, url)
+      end
+
+      parser.parse_feed response.body
+    end
+
+    def self.raise_response_error(response, url)
+      if looks_like_captcha?(response)
+        raise GotCaptcha.new("got captcha while trying to fetch #{url}")
+      else
+        raise ErrorServerResponse.new("Server returned error \"#{response.status} #{response.reason_phrase}\" url: #{url}")
+      end
+    end
+
+    def self.looks_like_captcha?(response)
+      return false unless response.status == 403
+      return false unless response.body.include?('captcha')
+      return false unless response.body.include?('Cloudflare Ray ID')
+      true
     end
 
     def self.fetcher
@@ -14,5 +35,9 @@ module Sources
     def self.parser
       Parser
     end
+
+    class Error < StandardError; end
+    class GotCaptcha < Error; end
+    class ErrorServerResponse < Error; end
   end
 end
