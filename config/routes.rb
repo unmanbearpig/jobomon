c83d8360d73e692852298e6b07d81bb152b0d73b
unmanbearpig <unmanbearpig@gmail.com>
2017-09-13 22:33:28 +0300

    Delete unused stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   resources :feeds, only: [:index, :create, :show]
   resources :sources, only: [:index, :create, :show]
-
-  get 'hello_world', to: 'hello_world#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
