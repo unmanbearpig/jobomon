3c77d4775fc3d99a18eb4f2e4a0c52b274d57285
unmanbearpig <unmanbearpig@gmail.com>
2017-09-16 13:02:40 +0300

    Add elm-bootstrap

--- elm-app/src/elm/View.elm
+++ elm-app/src/elm/View.elm
@@ -4,7 +4,12 @@ import Html exposing (..)
 import Msgs exposing (..)
 import Models exposing (..)
 import Html.Events exposing (onClick)
-
+import Bootstrap.CDN as CDN
+import Bootstrap.Grid as Grid
+import Bootstrap.Grid.Col as Col
+import Bootstrap.Grid.Row as Row
+import Bootstrap.Button as Button
+import Html.Attributes as Attr
 
 renderJobOffer : JobOffer -> Html Msg
 renderJobOffer offer =
@@ -13,7 +18,7 @@ renderJobOffer offer =
 
 jobOffersView : List JobOffer -> Html Msg
 jobOffersView offers =
-    ul [] (List.map renderJobOffer offers)
+    ul [ Attr.class "mt-4" ] (List.map renderJobOffer offers)
 
 
 renderLogMessage : String -> Html Msg
@@ -25,20 +30,34 @@ logView : List String -> Html Msg
 logView logMessages =
     div []
         [ h1 [] [ text "Log" ]
-        , ul [] (List.map renderLogMessage logMessages)
+        , ul [ Attr.class "mt-4" ] (List.map renderLogMessage logMessages)
         ]
 
 
 fetchJobOffersButton : Html Msg
 fetchJobOffersButton =
-    button [ onClick FetchJobOffers ] [ text "Refresh" ]
+    Button.button
+        [ Button.outlinePrimary
+        , Button.attrs
+            [ onClick FetchJobOffers
+            , Attr.class "mt-2"
+            ] ]
+        [ text "Refresh" ]
+
+
+mainContent : Model -> List (Html Msg)
+mainContent model =
+    [ h1 [] [ text ("Job offers") ]
+    , fetchJobOffersButton
+    , jobOffersView model.jobOffers
+    ]
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ h1 [] [ text ("Job offers") ]
-        , fetchJobOffersButton
-        , jobOffersView model.jobOffers
-        , logView model.log
+    Grid.container []
+        [ Grid.row [ Row.attrs [ Attr.class "mt-4" ] ]
+            [ Grid.col [ Col.sm8 ] (CDN.stylesheet :: (mainContent model))
+            , Grid.col [ Col.sm4 ] [ logView model.log ]
+            ]
         ]
